b844c052f36aa7a43956a7b44c8cafd1fc8a9f52
Ethan Schaezler <ethan26226@gmail.com>
2017-03-14 18:55:03 -0700

    fixed issue with missing variable

--- src/org/usfirst/frc/team2635/robot/commands/CommonCaluclations.java
+++ src/org/usfirst/frc/team2635/robot/commands/CommonCaluclations.java
@@ -4,6 +4,6 @@ import org.usfirst.frc.team2635.robot.RobotMap;
 
 public class CommonCaluclations {
 	public int calcEncCounts(double distance) {
-		return (int) (distance * RobotMap.DISTANCE_PER_WHEEL_REVOLUTION);
+		return (int) (distance * RobotMap.WHEEL_RADIUS_INCHES * RobotMap.CANTALON_ENCODER_PPR);
 	}
 }
